b542af628349d57463705a9d58d65782487f7eff
polyanskiyit <polyanskiyit@gmail.com>
2017-04-30 18:48:15 +0300

    first commit

---
+++ PlayMp3Project/MainActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PlayMp3Project
+{
+    [Activity(Label = "Mp3 Player Project", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        MediaPlayer player;
+
+        private List<string> mItem;
+        private ListView mListView;
+        private Button
+            playBtn,
+            pauseBtn,
+            stopBtn;
+        
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            init();
+
+            //  Єдиний робочий варіант
+            //player = MediaPlayer.Create(this, Resource.Raw.ABBA_L);
+
+
+            //  path
+            //string MP3Path = Path.Combine("/sdcard/" + Android.OS.Environment.DirectoryMusic);
+            var directories = Directory.EnumerateFiles(Path.Combine("/sdcard/" + Android.OS.Environment.DirectoryMusic));
+
+            mItem = new List<string>();
+            List<string> mItemName = new List<string>();
+
+            foreach (var directory in directories)
+            {
+                mItem.Add(directory);
+            }
+
+
+            //  ?????
+            player.SetDataSource(mItem[0]);
+
+
+
+
+
+
+
+            //  events
+            playBtn.Click += (sender, e) =>
+            {
+                player.Start();
+            };
+
+            pauseBtn.Click += (sender, e) =>
+            {
+                player.Pause();
+            };
+
+            stopBtn.Click += (sender, e) =>
+            {
+                player.Stop();
+            };
+
+            //  адаптер листа
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItem);
+            mListView.ItemClick += MListView_ItemClick;
+
+            mListView.Adapter = adapter;
+
+            
+        }
+        
+        public void init()
+        {
+            //  init
+            playBtn = FindViewById<Button>(Resource.Id.PlayButton);
+            pauseBtn = FindViewById<Button>(Resource.Id.pauseBtn);
+            stopBtn = FindViewById<Button>(Resource.Id.stopBtn);
+            mListView = FindViewById<ListView>(Resource.Id.listView1);
+        }
+
+        private void MListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            //Get our item from the list adapter
+            var item = this.mListView.GetItemAtPosition(e.Position);
+
+            //Make a toast with the item name just to show it was clicked
+            Toast.MakeText(this, item + " Clicked!", ToastLength.Short).Show();
+        }
+    }
+}
+
+
---
+++ PlayMp3Project/obj/Debug/android/src/md5f8070ba981fedaca9d07a57d5fb5d211/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f8070ba981fedaca9d07a57d5fb5d211;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("PlayMp3Project.MainActivity, PlayMp3Project, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("PlayMp3Project.MainActivity, PlayMp3Project, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
